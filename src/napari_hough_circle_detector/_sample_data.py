48bfb258a81429fb406a96e99f9b099f9a18dc12
faymanns <florian.aymanns@t-online.de>
2024-02-29 14:13:25 +0100

    Add sample data

--- src/napari_hough_circle_detector/_sample_data.py
+++ src/napari_hough_circle_detector/_sample_data.py
@@ -6,16 +6,57 @@ see: https://napari.org/stable/plugins/guides.html?#sample-data
 
 Replace code below according to your needs.
 """
+
 from __future__ import annotations
 
 import numpy
 
 
+def _add_circle(img, center, radius):
+    """
+    Draw a white (255) circle on the provided image.
+
+    Parameters
+    ----------
+    img : numpy.ndarray
+        2D array representing the image.
+    cetner : array-like
+        The x and y coordinates for the center
+        of the circle.
+    radius : float
+        The radius of the circle.
+
+    Returns
+    -------
+    img : numpy.ndarray
+        2D image array with additional circle.
+    """
+    height, width = img.shape
+    y, x = numpy.meshgrid(
+        numpy.arange(height), numpy.arange(width), indexing="ij"
+    )
+    y = y - center[0]
+    x = x - center[1]
+    r = numpy.sqrt(y**2 + x**2)
+    mask = r <= radius
+    img[mask] = 255
+    return img
+
+
 def make_sample_data():
-    """Generates an image"""
-    # Return list of tuples
-    # [(data1, add_image_kwargs1), (data2, add_image_kwargs2)]
-    # Check the documentation for more information about the
-    # add_image_kwargs
-    # https://napari.org/stable/api/napari.Viewer.html#napari.Viewer.add_image
-    return [(numpy.random.rand(512, 512), {})]
+    """
+    Generates a sample image with circles.
+
+    Returns
+    -------
+    List of tuples consiting of the data and a dictionary
+    holding the meta data of the sample image.
+    """
+    data = numpy.ones((512, 512), dtype=numpy.uint8) * 125
+    data = _add_circle(data, (100, 100), 50)
+    data = _add_circle(data, (120, 230), 70)
+    data = _add_circle(data, (400, 80), 20)
+    data = _add_circle(data, (300, 300), 100)
+    data = _add_circle(data, (200, 400), 30)
+
+    return [(data, {"name": "Sample circles"})]
