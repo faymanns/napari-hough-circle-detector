7e325b555dd31b1b9a8eba224b70901d7facb0a8
faymanns <florian.aymanns@t-online.de>
2024-03-04 11:24:02 +0100

    Test sample data generation

--- src/napari_hough_circle_detector/_tests/test_sample_data.py
+++ src/napari_hough_circle_detector/_tests/test_sample_data.py
@@ -1,7 +1,19 @@
-# from napari_hough_circle_detector import make_sample_data
+import numpy as np
 
-# add your tests here...
+from napari_hough_circle_detector import make_sample_data
 
 
-def test_something():
-    pass
+def test_sample_data():
+    sample_data = make_sample_data()
+
+    # Check the return type
+    assert isinstance(sample_data, list)
+
+    # Check that the list entry conforms to the LayerDataTuple
+    assert isinstance(sample_data[0], tuple)
+    assert isinstance(sample_data[0][0], np.ndarray)
+    assert isinstance(sample_data[0][1], dict)
+
+    # Check that image is 2D and non-empty
+    assert sample_data[0][0].ndim == 2
+    assert sample_data[0][0].min() > 0
